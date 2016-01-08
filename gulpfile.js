0e3fbf4aecbcc102765802d41bf1bd424c25db1d
hauptbenutzer <m.flucht@gmail.com>
2016-01-08 13:35:19 +0100

    Add gulpfile including serve and watch tasks.
    Requires `npm install`.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp        = require('gulp');
+var elm  		 = require('gulp-elm');
+var rename 	    = require("gulp-rename");
+var browserSync = require('browser-sync').create();
+
+gulp.task('elm', function () {
+    return gulp.src('Main.elm')
+        .pipe(elm.make())
+        .pipe(rename({basename: 'elm'}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('elm-watch', ['elm'], function(finish) {
+	browserSync.reload();
+	finish();
+});
+
+gulp.task('css-watch', function() {
+	return gulp.src('*.css')
+		.pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['elm'], function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.elm", ['elm-watch']);
+    gulp.watch('*.css', ['css-watch']);
+});
